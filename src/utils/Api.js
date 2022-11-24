71bed6b36105f2733c52ed6087450691774067be
saberothmane <44670693+saberothmane@users.noreply.github.com>
2022-11-24 16:26:08 +0100

    othmane

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const dev_url_ios = "http://127.0.0.1:3000/api/"
 const dev_url = "http://localhost:3000/api/"
-const prod_url = "http://185.225.232.250:3000/api/"
+const prod_url = "https://amomedbackend.com//api/"
 
 const Api = axios.create({
     baseURL: prod_url
