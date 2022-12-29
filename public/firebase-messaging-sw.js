638bafb07f413a292b5cf0a5b185c6d4ca08fbce
saberothmane <saberothmane431@gmail.com>
2022-12-29 15:29:55 +0100

    add notifcation for orders

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,30 +1,31 @@
-// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// // Initialize the Firebase app in the service worker by passing the generated config
-// var firebaseConfig = {
-//     apiKey: "AIzaSyAh-ftn6ggpTtZ_0qrVYDXaX5vqT_Pgm_E",
-//     authDomain: "amomed-acd60.firebaseapp.com",
-//     projectId: "amomed-acd60",
-//     storageBucket: "amomed-acd60.appspot.com",
-//     messagingSenderId: "322175485460",
-//     appId: "1:322175485460:web:05e9395b6ca7da553c4901",
-//     measurementId: "G-D5XEWDBFZS"
-// };
+// Initialize the Firebase app in the service worker by passing the generated config
+var firebaseConfig = {
+    apiKey: "AIzaSyAh-ftn6ggpTtZ_0qrVYDXaX5vqT_Pgm_E",
+    authDomain: "amomed-acd60.firebaseapp.com",
+    projectId: "amomed-acd60",
+    storageBucket: "amomed-acd60.appspot.com",
+    messagingSenderId: "322175485460",
+    appId: "1:322175485460:web:05e9395b6ca7da553c4901",
+    measurementId: "G-D5XEWDBFZS"
+};
 
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-// // Retrieve firebase messaging
-// const messaging = firebase.messaging();
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
 
-// messaging.onBackgroundMessage(function(payload) {
-//   console.log('Received background message ', payload);
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
 
-//   const notificationTitle = payload.notification.title;
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//   };
+  const notificationTitle = "nouvelle commande";
+  const notificationOptions = {
+    body: payload.data.message,
+    icon: './assets/layout/images/logo.png',
+  };
 
-//   self.registration.showNotification(notificationTitle,
-//     notificationOptions);
-// });
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
--- src/App.js
+++ src/App.js
@@ -12,39 +12,28 @@ import './assets/layout/layout.scss';
 import './App.scss';
 import '../src/css/global.css'
 import 'react-toastify/dist/ReactToastify.css'; // react toastify css
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
-import { onBackgroundMessage } from "firebase/messaging/sw";
 import {app} from './utils/firebase'
+import { SettingsService } from './service/SettingsService';
 
 const App = () => {
-    
+    const settingsService = new SettingsService()
     useEffect(() => {
         document.title = 'Amomed backoffice | Admin';
-        // const messaging = getMessaging(app);
-        // getTokenUser(messaging)
-        // onMessage(messaging, (payload) => {
-        //     console.log('Message received. ', payload);
-            // ...
-        // });
-        // onBackgroundMessage(messaging, (payload) => {
-        //     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-        //     // Customize notification here
-        //     const notificationTitle = 'Background Message Title';
-        //     const notificationOptions = {
-        //       body: 'Background Message body.',
-        //       icon: '/firebase-logo.png'
-        //     };
-          
-        //     self.registration.showNotification(notificationTitle,
-        //       notificationOptions);
-        // });
+        const messaging = getMessaging(app);
+        getTokenUser(messaging)
+        onMessage(messaging, (payload) => {
+            showNotification(payload.data.message)
+        })
+        
     }, []);
 
     function getTokenUser (messaging){
-        getToken(messaging, { vapidKey: 'BIKep2mn696ilTlsPXinooVOe4DZ-Ch_QdgZounbE36X2afH1zr9BMYUnslRiuEmHe3OOgmQT82W0nRwpJNrulo' }).then((currentToken) => {
+        getToken(messaging, { vapidKey: 'BIKep2mn696ilTlsPXinooVOe4DZ-Ch_QdgZounbE36X2afH1zr9BMYUnslRiuEmHe3OOgmQT82W0nRwpJNrulo' }).then(async(currentToken) => {
         if (currentToken) {
             console.log(currentToken)
+            addTokenToDatabase(currentToken)
             // Send the token to your server and update the UI if necessary
             // ...
         } else {
@@ -56,6 +45,37 @@ const App = () => {
         console.log('An error occurred while retrieving token. ', err);
         // ...
         });
+    }
+
+    async function addTokenToDatabase(currentToken){
+        const response = await settingsService.getAmomedInfo()
+        if(response.data){
+            let amomedInfo = {}
+            let tokenExists = false
+            if(response.data.length > 0) {
+                amomedInfo._id = response.data[0]._id  
+                if((response.data[0].tokens.filter((token) => token == currentToken)).length > 0)
+                    tokenExists = true
+            }
+            if(!tokenExists) {
+                amomedInfo.token = currentToken
+                await settingsService.addAmomedInfo(amomedInfo)
+            }
+        }
+    }
+
+    function showNotification (data){
+        toast(({ closeToast }) => <div><b>{data}</b></div>,{
+            type: 'success',
+            autoClose:false,
+            position: "top-right",
+            autoClose: false,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "light",
+    });
     }
 
     return (
